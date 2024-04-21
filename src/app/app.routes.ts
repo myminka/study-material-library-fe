4fd675c59ffb5327dca5de6acedade6042120fe3
Maksim Silkou <maxim.silkou.2002@gmail.com>
2024-04-21 17:17:56 +0200

    add new components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/general/home/home.component';
+import { LoginComponent } from './pages/general/login/login.component';
+import { SignupComponent } from './pages/general/signup/signup.component';
+import { AboutComponent } from './pages/general/about/about.component';
+import { ContractComponent } from './pages/general/contract/contract.component';
+import { NotFoundComponent } from './pages/general/not-found/not-found.component';
+import { StudyMaterialComponent } from './pages/general/study-material/study-material.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+
+    {path: 'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent},
+
+    {path: 'about', component: AboutComponent},
+    {path: 'contract', component: ContractComponent},
+    {path: 'materials', component: StudyMaterialComponent},
+
+    {path: '**', component: NotFoundComponent}
+];
---
+++ src/app/pages/general/study-material/study-material.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-study-material',
+  standalone: true,
+  imports: [],
+  templateUrl: './study-material.component.html',
+  styleUrl: './study-material.component.css'
+})
+export class StudyMaterialComponent {
+
+}
